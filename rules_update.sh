d7431bd2d5ff43d79e29fa623bacedf581bd2f78
root <root@vultr.guest>
2019-10-10 01:46:35 +0000

    更新规则

--- rules_update.sh
+++ rules_update.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 # 下载新版本规则文件
 cd /tmp
-wget -O daily.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/easylistchina.txt
-wget -O koolproxy.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/fanboy-annoyance.txt
-wget -O kp.dat https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/kp.dat
-wget -O user.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/user.txt
+#wget -O daily.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/easylistchina.txt
+wget -O koolproxy.txt https://raw.githubusercontent.com/koolproxy/merlin-koolproxy/master/koolproxy/koolproxy/data/rules/koolproxy.txt
+wget -O kp.dat https://raw.githubusercontent.com/koolproxy/merlin-koolproxy/master/koolproxy/koolproxy/data/rules/kp.dat
+#wget -O user.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/user.txt
 # 下载扩展规则合并
-wget -O user_tmp.txt https://gitee.com/hongh2/kpr_video_list/raw/master/kpr_video_list.txt
-wget -O daily_tmp.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/yhosts.txt
-cat user_tmp.txt >>user.txt
-cat daily_tmp.txt >>daily.txt
+#wget -O user_tmp.txt https://gitee.com/hongh2/kpr_video_list/raw/master/kpr_video_list.txt
+#wget -O daily_tmp.txt https://raw.githubusercontent.com/user1121114685/koolproxyR/master/koolproxyR/koolproxyR/data/rules/yhosts.txt
+#cat user_tmp.txt >>user.txt
+#cat daily_tmp.txt >>daily.txt
 
 mv -f /tmp/daily.txt /root/koolproxy
 mv -f /tmp/koolproxy.txt /root/koolproxy
@@ -25,7 +25,8 @@ day=`echo $time1|cut -b 7-8`
 hour=`echo $time1|cut -b 9-10`
 min=`echo $time1|cut -b 11-12`
 
-date1=$year-$month-$day" "$hour":"$min
+#date1=$year-$month-$day" "$hour":"$min
+date1=`date +%F" "%R`
 
 sed -i "3i\!x  -----update[video]: $date1" koolproxy.txt
 sed -i "3i\!x  -----update[rules]: $date1" koolproxy.txt
